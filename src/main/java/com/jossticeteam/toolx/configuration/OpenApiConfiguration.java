813245866ac30b67d52282744c1544c68e3188f3
UrielKillian <urielkillian2607@hotmail.com>
2021-05-03 14:25:36 -0500

    Base Project

---
+++ src/main/java/com/jossticeteam/toolx/configuration/OpenApiConfiguration.java
@@ -0,0 +1,19 @@
+package com.jossticeteam.toolx.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean(name = "toolxOpenApi")
+    public OpenAPI toolxOpenApi(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                .title("ToolX Application API")
+                .description("ToolX API implemented with Spring Boot RESTful service and document using springdoc-openapi and OpenAPI 3.0"));
+    }
+}
---
+++ src/main/java/com/jossticeteam/toolx/domain/model/User.java
@@ -0,0 +1,51 @@
+package com.jossticeteam.toolx.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User extends AuditModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 15)
+    @NaturalId
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 15, min = 5)
+    private String password;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 25)
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 25)
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 25)
+    private String email;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 25)
+    private String telephone;
+
+}
